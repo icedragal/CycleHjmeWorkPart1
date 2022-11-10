0a71fb86f8ed8846f25074a00665386a51777d5a
Icedragal <icedragal@gmail.com>
2022-11-10 15:45:26 +0300

    Задание 1.4

--- src/Main.java
+++ src/Main.java
@@ -18,5 +18,11 @@ public class Main {
         for (int i = 0; i < 17; i = i + 2) {
             System.out.println(i);
         }
+
+        //Task 1.4
+        System.out.println("Задание 1.4");
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
     }
 }
