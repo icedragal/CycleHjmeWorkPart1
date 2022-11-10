6f11ef0e82f5bfd814b436271e4dbe2adc59d601
Icedragal <icedragal@gmail.com>
2022-11-10 15:51:42 +0300

    Задание 2.1

--- src/Main.java
+++ src/Main.java
@@ -24,5 +24,11 @@ public class Main {
         for (int i = 10; i >= -10; i--) {
             System.out.println(i);
         }
+
+        //Task 2.1
+        System.out.println("Задание 2.1");
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            System.out.println(i + " год является високосным");
+        }
     }
 }
